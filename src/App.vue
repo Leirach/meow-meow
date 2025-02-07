23b08b5a4889c264cd8ae3b49abc8e000d0941ad
Leirach <juandleon27@gmail.com>
2025-02-07 04:27:45 -0600

    made meme

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import MovingButton from './components/MovingButton.vue'
+import { ref, onMounted } from "vue";
+
+const yippieVisible = ref(false);
+const audio = new Audio('/yippie.mp3'); // Set your MP3 file path
+
+const showYippie = () => {
+  if (yippieVisible.value) return;
+
+  yippieVisible.value = true;
+  audio.play();
+};
+
+onMounted(() => {
+  audio.load();
+  audio.volume = 0.5;
+  audio.addEventListener('ended', () => {
+    yippieVisible.value = false;
+  });
+});
+</script>
+
+<template>
+  <div class="container">
+    <img src="/meow.jpg" class="main-image" />
+    <h1>Will you be my meow meow?</h1>
+
+    <div class="buttons-container">
+      <button class="yes-button" @click="showYippie">Yes</button>
+      <div class="spacer"></div>
+      <MovingButton></MovingButton>
+    </div>
+    <div>
+      <img src="/meow2.jpg" class="main-image" />
+    </div>
+
+    <img v-if="yippieVisible" src="/yippie.gif" class="yippie" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #C88AC3;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 0;
+  height: 90vh;
+}
+
+button {
+  width: 150px;
+}
+
+.buttons-container {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+
+.spacer {
+  flex: 1
+}
+
+.yes-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border: none;
+  background-color: #C88AC3;
+  color: white;
+  border-radius: 5px;
+  filter: drop-shadow(0 0 1em #C88AC3);
+}
+
+.main-image {
+  width: 30vh;
+  height: auto;
+  margin-bottom: 20px;
+}
+
+.yippie {
+  position: absolute;
+  width: 100vw;
+  height: auto;
+}
+</style>
---
+++ src/components/MovingButton.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const x = ref(0);
+const y = ref(0);
+let count = ref(0);
+let buttonText = ref("No");
+
+let angryText = "No >:(";
+let veryAngryText = "Stop >:(((";
+
+const windowWidth = (window.innerWidth - 150) / 2;
+const windowHeight = (window.innerHeight - 250) / 2 ;
+
+const moveButton = () => {
+    console.log(window.innerWidth, window.innerHeight);
+    console.log("range x: ", -windowWidth, 2*windowWidth);
+    console.log("range y: ", -windowHeight, 2*windowHeight);
+
+    x.value = Math.random() * 2 * windowWidth - windowWidth - 100;
+    y.value = Math.random() * 2 * windowHeight - windowHeight -100;
+
+    console.log(x.value, y.value);
+
+    count.value++;
+    if (count.value >= 5) buttonText.value = angryText;
+    if (count.value >= 10) buttonText.value = veryAngryText;
+};
+
+</script>
+
+<template>
+    <button @mouseover="moveButton" @click="moveButton" class="moving-button"
+        :style="{ transform: `translate(${x}px, ${y}px)` }">
+        {{ buttonText }}
+    </button>
+</template>
+
+<style scoped>
+.moving-button {
+    position: relative;
+    transition: transform 0.2s ease-in-out;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border: none;
+    background-color: #9D152D;
+    color: white;
+    border-radius: 5px;
+    filter: drop-shadow(0 0 1em #9D152D);
+}
+</style>
